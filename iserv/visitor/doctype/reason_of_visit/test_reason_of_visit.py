c8e0d2bce1cf8784bc52be2502378072ecc880c2
Administrator <admin@example.com>
2025-01-16 14:42:39 +0300

    new folder structur has been pushed

---
+++ iserv/visitor/doctype/reason_of_visit/test_reason_of_visit.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, ADF and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testreasonofvisit(FrappeTestCase):
+	pass
---
+++ iserv/visitor/doctype/visitor_device/visitor_device.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ADF and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("visitor device", {
+// 	refresh(frm) {
+
+// 	},
+// });
